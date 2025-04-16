bf06648c4742b15868c77a2d257c0b8f7cc0127e
EthanSoroko <sorokoe@bc.edu>
2025-04-16 11:06:47 -0400

    Initial Commit

---
+++ BidNest/BidNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidNestApp.swift
+//  BidNest
+//
+//  Created by Ethan Soroko on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BidNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
